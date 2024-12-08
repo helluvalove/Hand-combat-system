6cbab02c385330984c908152c0f9813fccad4955
maryday <kisequ@vk.com>
2024-12-08 18:40:45 +0300

    add 2 forms logout and error login

--- handcombat.py
+++ handcombat.py
@@ -7,10 +7,10 @@ class LoginSystem(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        uic.loadUi('loginform.ui', self)
+        uic.loadUi('forms/loginform.ui', self)
 
         self.pushButton.clicked.connect(self.login)  
-        self.pushButton_2.clicked.connect(self.exit_system)  
+        self.pushButton_2.clicked.connect(self.logout)  
 
     def login(self):
         username = self.lineEdit.text()  
@@ -19,7 +19,30 @@ class LoginSystem(QDialog):
         if username == "admin" and password == "password":  
             QMessageBox.information(self, "Успех", "Вы успешно вошли в систему!")
         else:
-            QMessageBox.warning(self, "Ошибка", "Неверное имя пользователя или пароль!")
+            self.show_error_dialog()
+        
+    def logout(self):
+        logout_dialog = QDialog(self)
+        uic.loadUi('forms/logoutsystem.ui', logout_dialog)
+
+        da_button = logout_dialog.findChild(QtWidgets.QPushButton, "pushButton_2")
+        net_button = logout_dialog.findChild(QtWidgets.QPushButton, "pushButton")
+        if da_button:
+            da_button.clicked.connect(self.exit_system)
+        if net_button:
+            net_button.clicked.connect(logout_dialog.close)
+
+        logout_dialog.exec_()     
+
+    def show_error_dialog(self):
+        error_dialog = QDialog(self)
+        uic.loadUi('forms/errorlogin.ui', error_dialog)
+
+        ok_button = error_dialog.findChild(QtWidgets.QPushButton, "pushButton")
+        if ok_button:
+            ok_button.clicked.connect(error_dialog.close)
+
+        error_dialog.exec_()
 
     def exit_system(self):
         self.close()
