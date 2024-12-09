86e6c9384a2aa18b036caec5769754e18a4a7a49
maryday <kisequ@vk.com>
2024-12-09 11:20:37 +0300

    connect mainwindow after log sys

--- handcombat.py
+++ handcombat.py
@@ -2,7 +2,6 @@ import sys
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox
 
-
 class LoginSystem(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -16,11 +15,11 @@ class LoginSystem(QDialog):
         username = self.lineEdit.text()  
         password = self.lineEdit_2.text()  
 
-        if username == "admin" and password == "password":  
-            QMessageBox.information(self, "Успех", "Вы успешно вошли в систему!")
+        if username == "admin" and password == "password":
+            self.accept()
         else:
             self.show_error_dialog()
-        
+
     def logout(self):
         logout_dialog = QDialog(self)
         uic.loadUi('forms/logoutsystem.ui', logout_dialog)
@@ -47,7 +46,36 @@ class LoginSystem(QDialog):
     def exit_system(self):
         self.close()
 
-app = QApplication(sys.argv)
-window = LoginSystem()
-window.show()
-sys.exit(app.exec_())
+class MainWindow(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        uic.loadUi('forms/mainwindow.ui', self)
+
+        exit_button = self.findChild(QtWidgets.QPushButton, "pushButton")
+        if exit_button:
+            exit_button.clicked.connect(self.confirm_exit)
+
+        self.login_window = LoginSystem()
+        if self.login_window.exec_() != QDialog.Accepted:
+            return  
+        self.show()
+    
+    def confirm_exit(self):
+        logout_dialog = QDialog(self)
+        uic.loadUi('forms/logoutsystem.ui', logout_dialog)
+
+        yes_button = logout_dialog.findChild(QtWidgets.QPushButton, "pushButton_2")  
+        no_button = logout_dialog.findChild(QtWidgets.QPushButton, "pushButton")    
+
+        if yes_button:
+            yes_button.clicked.connect(self.close)
+        if no_button:
+            no_button.clicked.connect(logout_dialog.close)
+
+        logout_dialog.exec_()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    sys.exit(app.exec_())
